cf7e7c8de98923e3585763588414ab61fd3f80e8
Hackbright Student <user@mini-001.(none)>
2013-04-01 16:37:59 -0700

    load_users function working

--- seed.py
+++ seed.py
@@ -1,10 +1,26 @@
 import model
+from model import User
 import csv
 
 def load_users(session):
     # use u.user
-    pass
+    # open a file
+    f = open("user_test.txt")
+    fields = ["id", "age", "gender", "occupation", "zipcode"]
+    # read a line
+    for lines in f:
+        # parse a line
+        newline = lines.strip("\n").split("|")
+        # create an object
+        new_object = dict(zip(fields, newline))
+        print new_object
+        # add the object to a session
+        add_object = User(age=new_object['age'], zipcode=new_object['zipcode'])
+        session.add(add_object)
+        session.commit()
+        # commit
 
+    # repeat until done
 def load_movies(session):
     # use u.item
     pass
@@ -15,7 +31,11 @@ def load_ratings(session):
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    load_users(session)
+
+# def open_file(filename):
+#     f = open(filename).read()
+
 
 if __name__ == "__main__":
     s= model.connect()
