6399e696192d5ffc90f9b6afb7b1c2f0ad7e3777
Hackbright Student <user@mini-001.(none)>
2013-04-02 16:59:19 -0700

    DATA IS SEEDED

--- seed.py
+++ seed.py
@@ -1,13 +1,13 @@
 import model
 from model import User, Movies, Ratings
 import csv
-from datetime import datetime #, date
+from datetime import datetime, date
 import time
 
 def load_users(session):
     # use u.user
     # open a file
-    f = open("user_test.txt")
+    f = open("seed_data/u.user")
     fields = ["id", "age", "gender", "occupation", "zipcode"]
     # read a line
     for lines in f:
@@ -26,7 +26,7 @@ def load_users(session):
     # repeat until done
 def load_movies(session):
     # use u.item
-    f = open("movie_test.txt")
+    f = open("seed_data/u.item")
     fields = ['id', 'name', 'released_at','video_release' ,'imdb_url' ]
     for lines in f:
         newline = lines.strip("\n").split('|')
@@ -34,12 +34,14 @@ def load_movies(session):
         #sqlite3 cant properly display special chars even if it decoded properly, FLask will display it correctly
         name=new_object['name'].decode('latin-1')
         released_at = new_object['released_at']
-        struct_time = datetime.strptime(released_at, "%d-%b-%Y").date()
+        if released_at:
+            struct_time = datetime.strptime(released_at, "%d-%b-%Y").date()
+        else:
+            struct_time = None
         #decode needs to return to a variable
-        add_object = Movies(name=name, released_at=struct_time, imdb_url=new_object['imdb_url'])
+        add_object = Movies(id=new_object['id'], name=name, released_at=struct_time, imdb_url=new_object['imdb_url'])
         session.add(add_object)
     session.commit()
-
 
 def load_ratings(session):
     # use u.data
@@ -58,7 +60,7 @@ def load_ratings(session):
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    load_users(session)
+    #load_users(session)
     load_movies(session)
     #load_ratings(session)
 
